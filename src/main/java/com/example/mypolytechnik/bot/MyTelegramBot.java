9af7004607a33265aaee0d9a7aadb1d47e97fef5
Artem Krainyk <kraynyk_a2015@ukr.net>
2024-04-24 18:51:56 +0300

    з базою даних, кнопками і відправкою повідомлення кожної неділі в 00:00

--- src/main/java/com/example/mypolytechnik/bot/MyTelegramBot.java
+++ src/main/java/com/example/mypolytechnik/bot/MyTelegramBot.java
@@ -1,40 +1,61 @@
 package com.example.mypolytechnik.bot;
 
+import com.example.mypolytechnik.model.AdsRepository;
+import com.example.mypolytechnik.model.Info_Change_Schedule;
+import com.example.mypolytechnik.model.User;
+import com.example.mypolytechnik.model.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@EnableScheduling
 @Slf4j
 @Component
 public class MyTelegramBot extends TelegramLongPollingBot {
+    @Autowired
+    private UserRepository userRepository;
 
+    @Autowired
+    private AdsRepository adsRepository;
     private final String botUsername;
     private final String botToken;
 
-    static final String HELP_TEXT="Цей бот створений для студентів НУЛП (Дописати)";
+    static final String HELP_TEXT = "Цей бот створений для студентів НУЛП (Дописати)";
+    static final String ERROR_TEXT = "Error occurred: ";
+
     public MyTelegramBot(@Value("${bot.username}") String botUsername, @Value("${bot.token}") String botToken) {
         this.botUsername = botUsername;
         this.botToken = botToken;
-        List<BotCommand> listofCommands = new ArrayList<>();
-        listofCommands.add(new BotCommand("/start", "get a welcome message"));
-        listofCommands.add(new BotCommand("/mydata", "get your data stored"));
-        listofCommands.add(new BotCommand("/deletedata", "delete my data"));
-        listofCommands.add(new BotCommand("/help", "show help"));
-        listofCommands.add(new BotCommand("/settings", "show your preferences"));
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("start", "Початок роботи в боті"));
+        listOfCommands.add(new BotCommand("schedule", "Переглянути розклад"));
+        listOfCommands.add(new BotCommand("task", "Переглянути завдань"));
+        listOfCommands.add(new BotCommand("progress", "Переглянути успішність"));
+        listOfCommands.add(new BotCommand("reminder", "Переглянути нагадування"));
+        listOfCommands.add(new BotCommand("debt", "Переглянути заборгованість"));
+        listOfCommands.add(new BotCommand("help", "Показати довідку"));
         try {
-            this.execute(new SetMyCommands(listofCommands, new BotCommandScopeDefault(),null));
-        }catch(TelegramApiException e){
-            log.error("Error setting bot's command list: "+e.getMessage());
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Помилка встановлення списку команд бота: " + e.getMessage());
         }
     }
 
@@ -46,6 +67,7 @@ public class MyTelegramBot extends TelegramLongPollingBot {
 
             switch (messageText) {
                 case "/start":
+                    registerUser(update.getMessage());
                     sendStartMessage(chatId, update.getMessage().getChat().getFirstName());
                     break;
                 case "/help":
@@ -57,9 +79,26 @@ public class MyTelegramBot extends TelegramLongPollingBot {
         }
     }
 
+    private void registerUser(Message msg) {
+        if (userRepository.findById(msg.getChatId()).isEmpty()) {
+            var chatId = msg.getChatId();
+            var chat = msg.getChat();
+
+            User user = new User();
+            user.setChatId(chatId);
+            user.setFirstName(chat.getFirstName());
+            user.setLastName(chat.getLastName());
+            user.setUserName(chat.getUserName());
+            user.setRegistredAt(new Timestamp(System.currentTimeMillis()));
+
+            userRepository.save(user);
+            log.info("Користувач збережений: " + user);
+        }
+    }
+
     private void sendStartMessage(long chatId, String firstName) {
         String messageText = "Привіт, " + firstName + ", раді бачити тебе!";
-        log.info("Replied to user "+firstName);
+        log.info("Відповідь користувачу " + firstName);
         sendTextMessage(chatId, messageText);
     }
 
@@ -68,10 +107,30 @@ public class MyTelegramBot extends TelegramLongPollingBot {
         message.setChatId(String.valueOf(chatId));
         message.setText(text);
 
+        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("Розклад");
+        row.add("Завдання");
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("Успішність");
+        row.add("Нагадування");
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("Заборгованість");
+        row.add("Про чат-бот");
+        keyboardRows.add(row);
+
+        keyboardMarkup.setKeyboard(keyboardRows);
+
+        message.setReplyMarkup(keyboardMarkup);
+
         try {
             execute(message);
         } catch (TelegramApiException e) {
-            log.error("Error occurred"+e.getMessage());
+            log.error("Виникла помилка: " + e.getMessage());
         }
     }
 
@@ -83,5 +142,33 @@ public class MyTelegramBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return botToken;
+    }
+
+    private void executeMessage(SendMessage message){
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error(ERROR_TEXT + e.getMessage());
+        }
+    }
+
+    private void prepareAndSendMessage(long chatId, String textToSend){
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+        executeMessage(message);
+    }
+
+    @Scheduled(cron = "${cron.scheduler}")
+    private void sendAds(){
+        var ads=adsRepository.findAll();
+        var users=userRepository.findAll();
+
+        for(Info_Change_Schedule ad:ads){
+            for(User user:users){
+                prepareAndSendMessage(user.getChatId(),ad.getAd());
+            }
+        }
+
     }
 }
---
+++ src/main/java/com/example/mypolytechnik/model/AdsRepository.java
@@ -0,0 +1,6 @@
+package com.example.mypolytechnik.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdsRepository extends CrudRepository<Info_Change_Schedule,Long> {
+}
