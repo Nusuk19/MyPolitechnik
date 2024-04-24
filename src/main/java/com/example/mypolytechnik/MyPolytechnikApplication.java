ab446f9aabaa01ad6b47192ef6bbc5f331d54f61
Artem Krainyk <kraynyk_a2015@ukr.net>
2024-04-24 16:08:21 +0300

    без бази даних

--- src/main/java/com/example/mypolytechnik/MyPolytechnikApplication.java
+++ src/main/java/com/example/mypolytechnik/MyPolytechnikApplication.java
@@ -2,8 +2,7 @@ package com.example.mypolytechnik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import org.telegram.telegrambots.starter.TelegramBotStarterConfiguration;
+
 
 @SpringBootApplication
 public class MyPolytechnikApplication {
